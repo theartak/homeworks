fd80261fc49ace728076bfa68aea913b50972124
theartak <kartachok@gmail.com>
2022-02-22 13:05:27 +0400

    Day 14 Homework update

--- src/Day14Homework/Main.java
+++ src/Day14Homework/Main.java
@@ -227,6 +227,26 @@ public class Main {
     }
 
     /**
+     * Task 12
+     * A method that prints the amount of duplicate characters.
+     */
+
+    public static int dupeChars(String str) {
+        StringBuilder dupes = new StringBuilder();
+        int len = str.length();
+        for (int i = 0; i < len - 1; i++) {
+            for (int j = i + 1; j < len; j++) {
+                if ((str.charAt(i) == str.charAt(j)) &&
+                        !dupes.toString().contains(String.valueOf(str.charAt(i)))) {
+                    dupes.append(str.charAt(i));
+                    break;
+                }
+            }
+        }
+        return dupes.toString().length();
+    }
+
+    /**
      * Task 13
      * A method that returns the first unique character of the given string.
      */
