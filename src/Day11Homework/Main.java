054155466a95122a70c14d9d38d1ddc11b726f84
theartak <kartachok@gmail.com>
2022-02-23 16:45:53 +0400

    Day 11 Homework

--- src/Day11Homework/Main.java
+++ src/Day11Homework/Main.java
@@ -309,7 +309,8 @@ public class Main {
                 System.out.print("  " + i);
             }
             if (i >= 100 && i < 1000) {
-                System.out.print(" " + i);
+                System.out.print("" +
+                        " " + i);
             }
         }
         System.out.println();
