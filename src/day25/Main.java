3e6b05cff7f3eaa961d4d460ce6c1f3159ae58f1
theartak <kartachok@gmail.com>
2022-03-29 22:22:37 +0400

    Day 25 full update

--- src/day25/Main.java
+++ src/day25/Main.java
@@ -2,7 +2,7 @@ package day25;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(cutSymEms("abcdvbaK"));
     }
 
     /**
@@ -166,5 +166,89 @@ public class Main {
         return isPrime(n, i + 1);
     }
 
-    
+    /**
+     * Task 12
+     * A method that returns the largest digit in a decimal string.
+     */
+    public static int largestDigit(String str) {
+        if (str.length() == 1)
+            return Integer.parseInt(str);
+        if (str.charAt(0) > str.charAt(1))
+            return largestDigit(str.substring(1) + str.charAt(0));
+        return largestDigit(str.substring(1));
+    }
+
+    /**
+     * Task 13
+     * A method that returns the number of digits in a String
+     * consisting of letters and numbers.
+     */
+    public static int numberOfDigits(String str) {
+        if (str.length() == 0)
+            return 0;
+        try {
+            Integer.parseInt(str.substring(0, 1));
+            return 1 + numberOfDigits(str.substring(1));
+        } catch (NumberFormatException e) {
+            return numberOfDigits(str.substring(1));
+        }
+    }
+
+    /**
+     * Task 14
+     * A method that adds an asterisk between inch characters of a string.
+     */
+    public static String addAsterisk(String str, int length) {
+        if (length == str.length() - 1) {
+            return str.charAt(length) + "";
+        }
+        return str.charAt(length) + "*" + addAsterisk(str, length + 1);
+    }
+
+    /**
+     * Task 15
+     * A method that adds parentheses in a string in the following way:
+     * "example" -> "(e (x (a (m) p) l) e)"
+     */
+    public static String addParentheses(String str, int n) {
+        if (str.length() == 0)
+            return ")";
+
+        if (str.length() > n / 2) {
+            return '(' + "" + str.charAt(0) + " " + addParentheses(str.substring(1), n);
+        }
+
+        if (str.length() == n / 2 && n % 2 == 0) {
+            return str.charAt(0) + addParentheses(str.substring(1), n);
+        }
+        return ')' + " " + str.charAt(0) + addParentheses(str.substring(1), n);
+    }
+
+    /**
+     * Task 16
+     * A method that adds a "mirror" to the given string.
+     */
+    public static String mirroredString(String str) {
+        return str + mirrorString(str);
+    }
+
+    private static String mirrorString(String str) {
+        if (str == null || str.length() <= 1) {
+            return str;
+        }
+        return mirrorString(str.substring(1)) + str.charAt(0);
+    }
+
+    /**
+     * Task 17
+     * A method that cuts symmetric identical elements from a given string.
+     */
+    public static String cutSymEms(String str) {
+        if (str.length() == 1)
+            return str;
+        if (str.charAt(0) == str.charAt(str.length() - 1)) {
+            return cutSymEms(str.substring(1, str.length() - 1));
+        }
+        return str.charAt(0) + cutSymEms(str.substring(1, str.length() - 1)) + str.charAt(str.length() - 1);
+    }
 }
