7c93f7e44562bba29d09c3c3c37e178c99b23698
theartak <kartachok@gmail.com>
2022-03-30 10:28:49 +0400

    Day 25 full update

--- src/day25/Main.java
+++ src/day25/Main.java
@@ -2,7 +2,7 @@ package day25;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(cutSymEms("abcdvbaK"));
+        System.out.println(cutSymEms("abcdvba"));
     }
 
     /**
